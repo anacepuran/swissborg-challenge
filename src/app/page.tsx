68aec967902882b095774bff3a520615a1ecf4f7
ana <45954676+anacepuran@users.noreply.github.com>
2024-04-30 12:20:46 +0200

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import BorgTokenPage from "@/pages/BorgTokenPage";
-
-export default function Home() {
-  return (
-    <main className="flex flex-col gap-8 min-h-screen items-center bg-white">
-      <BorgTokenPage />
-    </main>
-  );
-}
