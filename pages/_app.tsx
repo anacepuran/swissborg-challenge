57c37ff57e487120c145cac6371ae256e5f0c3ae
ana-cepuran <ana.cepuran@centric.eu>
2024-04-30 13:51:15 +0200

    refactor and improve twitter tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,23 +2,31 @@ import "@/styles/globals.css";
 import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
 
+const TITLE = "SwissBorg - BORG Token Metrics";
+const DESCRIPTION =
+  "Deep-dive into the statistics of BORG and the mechanics of the full SwissBorg Ecosystem.";
+const SITE_URL = "https://swissborg-challenge.vercel.app";
+const IMAGE_URL =
+  "https://swissborg-challenge.vercel.app/images/shareable-url.png";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <title>{TITLE}</title>
       <DefaultSeo
-        title="SwissBorg - BORG Token Metrics"
-        description="Deep-dive into the statistics of BORG and the mechanics of the full SwissBorg Ecosystem."
+        title={TITLE}
+        description={DESCRIPTION}
         openGraph={{
           type: "website",
           locale: "en_IE",
-          url: "https://swissborg-challenge.vercel.app",
-          site_name: "SwissBorg - BORG Token Metrics",
+          url: SITE_URL,
+          site_name: TITLE,
           images: [
             {
-              url: "https://swissborg-challenge.vercel.app/images/shareable-url.png",
+              url: IMAGE_URL,
               width: 1200,
               height: 630,
-              alt: "BORG Token Metrics",
+              alt: TITLE,
             },
           ],
         }}
@@ -28,15 +36,9 @@ export default function App({ Component, pageProps }: AppProps) {
           cardType: "summary_large_image",
         }}
       />
-      <meta name="twitter:title" content="SwissBorg - BORG Token Metrics" />
-      <meta
-        name="twitter:description"
-        content="Deep-dive into the statistics of BORG and the mechanics of the full SwissBorg Ecosystem."
-      />
-      <meta
-        name="twitter:image"
-        content="https://swissborg-challenge.vercel.app/images/shareable-url.png"
-      />
+      <meta name="twitter:title" content={TITLE} />
+      <meta name="twitter:description" content={DESCRIPTION} />
+      <meta name="twitter:image" content={IMAGE_URL} />
       <meta name="twitter:image:width" content="1200" />
       <meta name="twitter:image:height" content="630" />
       <Component {...pageProps} />;
