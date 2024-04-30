8a48155c98294dff1722fe5a903023bf472bdf0d
ana-cepuran <ana.cepuran@centric.eu>
2024-04-30 11:17:52 +0200

    refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { NextSeo } from "next-seo";
 import { Inter } from "next/font/google";
 import "./style/globals.css";
 
@@ -17,6 +18,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <NextSeo
+        title="BORG Token Metrics and Breakdown Page"
+        description="Deep-dive into the statistics of BORG and the mechanics of the full SwissBorg Ecosystem."
+        openGraph={{
+          title: "BORG Token Metrics and Breakdown Page",
+          description:
+            "Deep-dive into the statistics of BORG and the mechanics of the full SwissBorg Ecosystem.",
+          images: [
+            {
+              url: "https://anacepuran.github.io/public/shareable-url.png",
+              width: 1200,
+              height: 630,
+              alt: "BORG Token Metrics and Breakdown Page Image",
+            },
+          ],
+          url: "https://swissborg-challenge.vercel.app/",
+          type: "website",
+        }}
+        twitter={{
+          cardType: "summary_large_image",
+        }}
+      />
       <body className={inter.className} style={{ fontFamily: "TT Commons" }}>
         {children}
       </body>
--- swissborg-tech-challenge/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/swissborg-challenge",
-  output: "export",
-  reactStrictMode: true,
-};
-
-export default nextConfig;
