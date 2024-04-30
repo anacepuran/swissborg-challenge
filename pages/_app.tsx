95d32d7fa8566316f7478596455076ff1876aea2
ana-cepuran <ana.cepuran@centric.eu>
2024-04-30 13:09:34 +0200

    added shareable url

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,34 @@
 import "@/styles/globals.css";
+import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <DefaultSeo
+        title="SwissBorg - BORG Token Metrics"
+        description="Deep-dive into the statistics of BORG and the mechanics of the full SwissBorg Ecosystem."
+        openGraph={{
+          type: "website",
+          locale: "en_IE",
+          url: "https://swissborg-challenge.vercel.app",
+          site_name: "SwissBorg - BORG Token Metrics",
+          images: [
+            {
+              url: "https://swissborg-challenge.vercel.app/shareable-url.png",
+              width: 1200,
+              height: 630,
+              alt: "BORG Token Metrics",
+            },
+          ],
+        }}
+        twitter={{
+          handle: "@handle",
+          site: "@site",
+          cardType: "summary_large_image",
+        }}
+      />
+      <Component {...pageProps} />;
+    </>
+  );
 }
