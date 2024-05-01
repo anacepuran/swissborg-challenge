38190c3470d29c731f512a4dff15e20b4917bd71
ana-cepuran <ana.cepuran@centric.eu>
2024-05-01 12:52:39 +0200

    configuration

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,13 @@ import bundleAnalyzer from "@next/bundle-analyzer";
 
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    if (config.optimization.splitChunks) {
+      // Set the minSize property for splitChunks
+      config.optimization.splitChunks.minSize = 50000; // size in bytes
+    }
+    return config;
+  },
   ...bundleAnalyzer({
     enabled: process.env.ANALYZE === "true",
   }),
