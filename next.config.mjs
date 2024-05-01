f8292f7c260f99a080216e014c75ddbf32929e4a
ana-cepuran <ana.cepuran@centric.eu>
2024-05-01 14:24:52 +0200

    test

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import bundleAnalyzer from "@next/bundle-analyzer";
 
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    modern: true, // Enable modern mode for differential serving
+  },
   ...bundleAnalyzer({
     enabled: process.env.ANALYZE === "true",
   }),
