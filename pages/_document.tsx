a8956d0c2d793492d357f1205f8e2dfdff663808
ana-cepuran <ana.cepuran@centric.eu>
2024-05-01 13:58:20 +0200

    change config

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script type="module" src="/modern-bundle.js" async />
+        <script noModule src="/legacy-bundle.js" async />
+      </Head>
       <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       <body>
         <Main />
