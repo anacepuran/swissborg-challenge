995df47d8b28f573dc79a275db7fdebb1eadf91a
ana-cepuran <ana.cepuran@centric.eu>
2024-05-03 10:45:46 +0200

    favicon???

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,8 +5,6 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <link rel="icon" type="image/x-icon" href="images/favicon.ico" />
-      <link rel="icon" type="image/x-icon" href="public/images/favicon.ico" />
-      <link rel="icon" type="image/svg+xml" href="public/images/favicon.ico" />
       <body>
         <Main />
         <NextScript />
